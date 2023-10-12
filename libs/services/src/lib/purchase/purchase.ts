ce0abda6bfa80067107a7e0c8d5e3a41befce8b7
Victor Yurkin <victor.yurkin@naxgrp.com>
2023-10-12 11:48:04 -0400

    Updated api url

--- libs/services/src/lib/purchase/purchase.ts
+++ libs/services/src/lib/purchase/purchase.ts
@@ -2,6 +2,8 @@ import { get } from '@idme/clients-http';
 import { Purchase } from '@idme/models';
 
 export const getPurchases = async (): Promise<Purchase[]> => {
-  const response = await get('/api/data.json');
+  const response = await get(
+    'https://victoryurkineng.github.io/purchases/app/api/data.json'
+  );
   return response as Purchase[];
 };
